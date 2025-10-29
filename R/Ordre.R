f31d7c4260fbb07658634f32a5de2727fa11bbef
Dylan-cvn <d.cavin@outlook.com>
2025-10-29 10:01:58 +0100

    Update documentation and function name to 'Ordre'

--- R/Ordre.R
+++ R/Ordre.R
@@ -1,7 +1,7 @@
 #' Trier un vecteur (équivalent de `sort`)
 #'
 #' @description
-#' Trie un vecteur comme `base::sort()`. Tu peux ensuite faire `ordre(x)[k]`
+#' `Ordre()` trie un vecteur comme `base::sort()`. Tu peux faire `Ordre(x)[k]`
 #' pour obtenir la k-ième statistique d'ordre.
 #'
 #' @param x Vecteur à trier.
@@ -9,20 +9,15 @@
 #' @param na.last NA/TRUE/FALSE : où placer les NA.
 #' @param partial Entier(s) pour tri partiel (comme dans `sort()`).
 #' @param ... Autres arguments passés à `base::sort()`.
+#'
 #' @return Le vecteur trié.
 #'
 #' @examples
 #' x <- c(5, 2, 9, 1)
-#' ordre(x)      # 1 2 5 9
-#' ordre(x)[3]   # 5
+#' Ordre(x)      # 1 2 5 9
+#' Ordre(x)[3]   # 5
 #'
 #' @export
-ordre <- function(x, decreasing = FALSE, na.last = NA, partial = NULL, ...) {
+Ordre <- function(x, decreasing = FALSE, na.last = NA, partial = NULL, ...) {
   base::sort(x, decreasing = decreasing, na.last = na.last, partial = partial, ...)
 }
-
-#' Alias de `ordre()`
-#'
-#' @rdname ordre
-#' @export
-Ordre <- ordre
